a8015cf5e01ee5679d623f42df61f5aae8393b45
AlexaGutieMora <143145315+AlexaGutieMora@users.noreply.github.com>
2023-09-10 03:02:52 +0000

    09 sept

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,8 @@ int main(int argc, char const *argv[])
     m1.Jugar();
     m1.Jugar();
 
-    std::cout
-    <<m1.DecirNombre() << " tiene "
-    << m1.DecirHambre() << "de hambre. "
-
+    std::cout <<m1.DecirNombre() << " tiene " << m1.DecirHambre() << "de hambre. ";
+    
     m1.Comer(5);
     
     return 0;
